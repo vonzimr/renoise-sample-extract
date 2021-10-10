f70a6942e84fb0bf56d15aa8cfc1cf68a8463e52
vonzimr <vonzimr@gmail.com>
2021-10-09 23:18:31 -0700

    feat: progress bar (bad error handling)

--- src/convert.py
+++ src/convert.py
@@ -1,25 +1,38 @@
 from typing import IO, List, Tuple
 import click
-from pydub import AudioSegment
+from pydub import AudioSegment, exceptions
 from pathlib import Path
 from zipfile import ZipFile, Path as ZipPath
 import os
 from os.path import splitext
 import re
+import sys
+
+import pydub
 
 XRNI_SAMPLE_PATH = 'SampleData/'
+name_extract = re.compile("Sample[0-9]+ \(?([^\(\)]*)")
+
 
 def get_sample_data(sample: IO[bytes]):
     full_path = sample.name
     stem, ext = splitext(full_path)
-    name = stem.replace(XRNI_SAMPLE_PATH, '')
-    ext = ext.replace('.', '')
-    return {'path': full_path, 'name': name, 'ext': ext, 'sample': sample}
+    try:
+        name = name_extract.search(stem).group(1)
+    except AttributeError:
+        sys.exit(f"Could not extract name from {full_path}")
+    else:
+        ext = ext.replace('.', '')
+        return {'path': full_path, 'name': name, 'ext': ext, 'sample': sample}
 
 
 def get_audio_from_xrns(xrni):
     zip = ZipFile(xrni)
-    return [get_sample_data(zip.open(sample)) for sample in zip.namelist() if XRNI_SAMPLE_PATH in sample]
+
+    def is_sample(path):
+        return XRNI_SAMPLE_PATH in path and path.replace(XRNI_SAMPLE_PATH, '') != ''
+
+    return [get_sample_data(zip.open(sample)) for sample in zip.namelist() if is_sample(sample)]
 
 
 def create_export_folder(out='export'):
@@ -39,7 +52,6 @@ def convert(file_or_path, out, mono, bit_depth):
     file_or_path = Path(file_or_path)
 
     def convert_instrument(xrni: Path):
-        click.echo(f"Reading XRNI {xrni.name}...")
         sample_data = get_audio_from_xrns(xrni)
 
         if(len(sample_data) == 0):
@@ -51,24 +63,25 @@ def convert(file_or_path, out, mono, bit_depth):
         if not save_destination.exists():
             os.mkdir(save_destination)
 
-
-
-        click.echo(f'Found {len(sample_data)} samples')
-
         for data in sample_data:
-            click.echo(f"Converting {data['name']}...")
-            audio = AudioSegment.from_file(data['sample'], format=data['ext'])
-            audio = audio.set_channels(1 if mono else audio.channels)
-            audio = audio.set_sample_width(int(int(bit_depth) / 8))
-            audio.export(export_folder.joinpath(
-                f"{xrni.stem}/{data['name']}.wav"), format='wav')
-            click.echo(f'Done.')
-
+            try:
+                audio = AudioSegment.from_file(
+                    data['sample'], format=data['ext'])
+            except exceptions.PydubException:
+                click.clear()
+                click.echo(
+                    f"encountered an error decoding {data['name']}", err=True)
+            else:
+                audio = audio.set_channels(1 if mono else audio.channels)
+                audio = audio.set_sample_width(int(int(bit_depth) / 8))
+                audio.export(export_folder.joinpath(
+                    f"{xrni.stem}/{data['name']}.wav"), format='wav')
 
     def convert_folder(search_path: Path):
-        xrni_files = search_path.glob('**/*.xrni')
-        for file in xrni_files:
-            convert_instrument(file)
+        xrni_files = [file for file in search_path.glob('**/*.xrni')]
+        with click.progressbar(xrni_files, label=f"Extracting Samples...") as bar:
+            for file in bar:
+                convert_instrument(file)
 
     if file_or_path.is_dir():
         return convert_folder(file_or_path)
